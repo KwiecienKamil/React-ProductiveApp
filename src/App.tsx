07327bf85412d793b13da442a5344d57d599a6c9
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2024-11-02 17:27:10 +0100

    Test

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { useState } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+  axios.get("http://192.168.100.8:8081/users").then((res) => {
+    const dataFromDB = res.data;
+    const dataForClient = res.data[0].username;
+    setData(dataForClient);
+  });
+
+  return <div className="">{data}</div>;
+}
+
+export default App;
