6b22509c011720c7794782a3eac6f22431261b05
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2024-11-02 20:55:46 +0100

    Tested2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [data, setData] = useState([]);
-  axios.get("http://192.168.100.8:8081/users").then((res) => {
-    const dataForClient = res.data[0].username;
-    setData(dataForClient);
-  });
 
-  return <div className="">{data}</div>;
+  useEffect(() => {
+    axios
+      .get("https://6699-77-253-29-247.ngrok-free.app/users", {
+        headers: {
+          "ngrok-skip-browser-warning": "true",
+        },
+      })
+      .then((res) => {
+        const responseForClient = res.data[0].username;
+        setData(responseForClient);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return <div>{data}</div>;
 }
 
 export default App;
